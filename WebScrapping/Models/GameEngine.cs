ab7e6cc85785490b3cc74bf36af6d98d1df4f4e5
Hernan Di Tano <hditano@gmail.com>
2023-05-01 16:17:36 +0200

    Add project files.

---
+++ WebScrapping/Models/GameEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapping.Models
+{
+    public class GameEngine
+    {
+        public string Name { get; set; }
+        public int Employees { get; set; }
+        public string Engine { get; set; }
+        public string Games { get; set; }
+
+    }
+}
---
+++ WebScrapping/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+
+
+var html = @"https://gist.github.com/raysan5/909dc6cf33ed40223eb0dfe625c0de74";
+
+HtmlWeb web = new HtmlWeb();
+
+var htmlDoc = web.Load(html);
+
+var TitleTable = htmlDoc.DocumentNode.SelectNodes("//*[@id='file-custom_game_engines_small_study-md-readme']/article/table[2]/tbody/tr[1]/td[3]");
+
+Console.WriteLine("------------------");
+
+foreach (var title in TitleTable)
+    Console.WriteLine(title.InnerText);
