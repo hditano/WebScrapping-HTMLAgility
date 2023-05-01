dbc2593d12fcf267a4faa10c07b8dd3432d6ee55
Hernan Di Tano <hditano@gmail.com>
2023-05-02 00:50:55 +0200

    Refactor parsing HTML

--- WebScrapping/Program.cs
+++ WebScrapping/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HtmlAgilityPack;
+using System.Runtime.InteropServices.ComTypes;
 using WebScrapping.Models;
 
 var html = @"https://gist.github.com/raysan5/909dc6cf33ed40223eb0dfe625c0de74";
@@ -8,13 +9,13 @@ HtmlWeb web = new HtmlWeb();
 
 var htmlDoc = web.Load(html);
 
-var TitleTable = htmlDoc.DocumentNode.SelectNodes("//*[@id='file-custom_game_engines_small_study-md-readme']/article/table[2]/tbody/tr[1]");
+var TitleTable = htmlDoc.DocumentNode.SelectNodes("//tbody/tr");
 
-var engines = new List<GameEngine>();
+var gameEngines = new List<GameEngine>();
 
-foreach (var row in TitleTable)
+foreach(var row in TitleTable)
 {
-    var gameEngine = new GameEngine
+    var game = new GameEngine
     {
         Name = row.SelectSingleNode("./td[1]").InnerText,
         Employees = row.SelectSingleNode("./td[2]").InnerText,
@@ -22,12 +23,8 @@ foreach (var row in TitleTable)
         Games = row.SelectSingleNode("./td[4]").InnerText
     };
 
-    engines.Add(gameEngine);
+    gameEngines.Add(game);
 }
 
-
-foreach(var item in engines)
-{
-    Console.WriteLine($"{item.Name}");
-    Console.WriteLine($"{item.Engine}");
-}
+foreach(var engine in gameEngines)
+    Console.WriteLine(engine.Games);
